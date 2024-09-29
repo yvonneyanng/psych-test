b67c45bff96a598e52ee76278c40a7f4c0bcdddc
Yvonne Yang <yvnyang@icloud.com>
2024-09-30 01:37:10 +1000

    fix - landing page

--- components/Description.tsx
+++ components/Description.tsx
@@ -10,18 +10,20 @@ export default function Description({
 }: DescriptionProps) {
   const t = useTranslations("Landing");
   return (
-    <div className="flex flex-col items-center justify-start h-[100dvh] bg-white">
-      <ProgressBar progress={progress} onGoToLanding={onGoToLanding} />
-      <div className="flex flex-col flex-1 items-center justify-center">
-        <div className="bg-zinc-300 h-64 w-60 flex justify-center items-center">
-          {t("description")}
+    <div className="relative flex flex-col items-center justify-start min-h-[100dvh] max-w-screen-xs md:pt-20 bg-custom">
+      <div className="relative z-10 flex flex-col items-center justify-start space-y-3">
+        <ProgressBar progress={progress} onGoToLanding={onGoToLanding} />
+        <div className="flex flex-col flex-1 items-center justify-center">
+          <div className="bg-zinc-300 h-64 w-60 flex justify-center items-center">
+            {t("description")}
+          </div>
+          <button
+            onClick={onStartQuiz}
+            className="bg-zinc-300 text-xl text-black px-6 py-3 rounded-full mt-5"
+          >
+            {t("startButton")}
+          </button>
         </div>
-        <button
-          onClick={onStartQuiz}
-          className="bg-zinc-300 text-xl text-black px-6 py-3 rounded-full mt-5"
-        >
-          {t("startButton")}
-        </button>
       </div>
     </div>
   );
